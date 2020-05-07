eb204cfef57cf5a9ad1c9e3536f0ea850fe92893
Jarosław Żak <shado.ackerly@gmail.com>
2020-05-07 18:25:23 +0200

    initial 2

---
+++ ShoppingApp/ShoppingApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ShoppingApp.Models;
+
+namespace ShoppingApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
