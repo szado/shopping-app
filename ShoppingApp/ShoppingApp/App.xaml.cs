7e8d4e3e65983b0c41b6748d02601d5a361e078c
zer0xday <majcher10@gmail.com>
2020-05-15 20:17:37 +0200

    Rm: unnecessary

--- ShoppingApp/ShoppingApp/App.xaml.cs
+++ ShoppingApp/ShoppingApp/App.xaml.cs
@@ -23,30 +23,7 @@ namespace ShoppingApp
 
         protected async override void OnStart()
         {
-            var rxcui = "198440";
-            var request = HttpWebRequest.Create(string.Format(@"https://rxnav.nlm.nih.gov/REST/RxTerms/rxcui/{0}/allinfo", rxcui));
-            request.ContentType = "application/json";
-            request.Method = "GET";
 
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            {
-                if (response.StatusCode != HttpStatusCode.OK)
-                    Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                {
-                    var content = reader.ReadToEnd();
-                    if (string.IsNullOrWhiteSpace(content))
-                    {
-                        Console.Out.WriteLine("Response contained empty body...");
-                    }
-                    else
-                    {
-                        Console.Out.WriteLine("Response Body: \r\n {0}", content);
-                    }
-
-                    Assert.NotNull(content);
-                }
-            }
         }
 
         protected override void OnSleep()
